f20eb75a41bf0d041694db6a93843fcb7344f42e
anastasiia.medinova <anastasiia.medinova@gamesys.ee>
2022-10-30 17:46:33 +0200

    TASK-006: Implement paytable with a blink animation

--- src/presenters/paytable.presenter.ts
+++ src/presenters/paytable.presenter.ts
@@ -2,30 +2,27 @@ import { injectable, inject, postConstruct } from 'inversify';
 
 import { APP_TYPES } from '../types';
 import { GarbageBag, GarbageCollect } from '../components';
-import { UiControlView } from '../views';
+import { PaytableView } from '../views';
 import { AppFlowModel } from '../model';
 
 @injectable()
-export class UiControlPresenter implements GarbageCollect {
+export class PaytablePresenter implements GarbageCollect {
   protected readonly _garbageBag = new GarbageBag();
 
   constructor(
-    @inject(APP_TYPES.UiControlView) private readonly _iControlView: UiControlView,
+    @inject(APP_TYPES.PaytableView) private readonly _paytableView: PaytableView,
     @inject(APP_TYPES.AppFlowModel) private readonly _appFlowModel: AppFlowModel
   ) {
-    const { clickSpinButton$ } = this._iControlView;
     const { action$ } = this._appFlowModel;
 
-    this._garbageBag.completable$(clickSpinButton$).subscribe(() => {
-      this._appFlowModel.call({ type: 'SPIN_START' });
-    });
-
     this._garbageBag.completable$(action$).subscribe((action) => {
-      if (action.type === 'SPIN_COMPLETE') {
-        this._iControlView.toggleButtonState();
-      }
-      if (action.type === 'SPIN_RESULT') {
-        this._iControlView.updateBalance(action.data);
+      switch (action.type) {
+        case 'SPIN_START':
+          this._paytableView.stopBlinking();
+          break;
+        case 'SPIN_RESULT':
+          this._paytableView.startBlinking(action.data);
+          break;
       }
     });
   }
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,3 +1,4 @@
 export { AppView } from './app.view';
 export { PaylinesView } from './paylines.view';
 export { UiControlView } from './ui-contol.view';
+export { PaytableView } from './paytable.view';
